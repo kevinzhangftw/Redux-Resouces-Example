3649d705094dc89a246186be8725fcdc38db5699
Kai Kevin <kevin.c.t.zhang@gmail.com>
2018-09-26 14:24:34 -0700

    matt demoed compose and it works

--- MainScreen/MainScreenContainer.js
+++ MainScreen/MainScreenContainer.js
@@ -1,18 +1,12 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux'
-import MainScreen from './MainScreen'
-// import fetchQuizzes from '../actions/quizzes'
-import fetchResources from '../actions/resources'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose, withHandlers } from 'recompose';
+import MainScreen from './MainScreen';
+import { fetchQuizzes } from '../actions/quizzes';
 
-type Props = {};
-class MainScreenContainer extends Component<Props> {
-  render() {
-    const queryString = `https://opentdb.com/api.php?amount=5&difficulty=hard&type=boolean`
-    const handlePress = () => this.props.dispatch(fetchResources('quizzes', 'fetchQuizzes', queryString))
-    return (
-        <MainScreen onPress={handlePress} />
-    );
-  }
-}
-
-export default connect()(MainScreenContainer)
+export default compose(
+  connect(null, { fetchQuizzes }),
+  withHandlers({
+    onPress: ({fetchQuizzes}) => fetchQuizzes
+  })
+)(MainScreen)
